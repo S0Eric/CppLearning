db41a7d36f4ce84c2bb747e6cf0687fcf4540c60
Eric Schmucker <eric@snowscape.com>
2024-03-05 00:30:48 -0700

    Minor simplification

--- StdContainers/main.cpp
+++ StdContainers/main.cpp
@@ -4,7 +4,16 @@
 #include "StdDeque.h"
 #include "StdVector.h"
 
-//#include "LogMemoryOperations.h"
+//void* operator new(size_t size) {
+//    auto ret = malloc(size);
+//    std::cout << "Allocated " << size << " bytes: " << std::hex << ret << std::dec << std::endl;
+//    return ret;
+//}
+//
+//void operator delete(void* memory, size_t size) {
+//    std::cout << "Freeing " << size << " bytes: " << std::hex << memory << std::dec << std::endl;
+//    free(memory);
+//}
 
 int main() {
     //StdForwardList::DoBasics();
